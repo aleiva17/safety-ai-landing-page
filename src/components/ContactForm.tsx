e6ea4a1cd5ecd5df894f11e46eec761e36d3f341
aleiva <aleiva1700@gmail.com>
2022-11-15 19:42:30 -0500

    Updated: Remove form autocomplete

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -14,20 +14,20 @@ const ContactForm = (): JSX.Element => {
   return (
     <form className="bg-[#1a202c] flex flex-col max-w-full px-9 md:px-10 pt-10 rounded-3xl gap-4">
       <div className="flex flex-wrap md:flex-nowrap justify-between gap-4">
-        <label className="text-white text-xl" htmlFor="name">Nombre</label>
-        <input onChange={ (event) => handleOnChangeNameInput(event.target.value) } className="w-full md:w-72 max-w-72 rounded-full py-1 px-3" type="text" name="name" value={ name } />
+        <label className="text-white text-xl" htmlFor="nForm">Nombre</label>
+        <input required autoComplete="off" onChange={ (event) => handleOnChangeNameInput(event.target.value) } className="w-full md:w-72 max-w-72 rounded-full py-1 px-3" type="text" name="nForm" value={ name } />
       </div>
       <div className="flex flex-wrap md:flex-nowrap justify-between gap-4">
-        <label className="text-white text-xl" htmlFor="phone">Celular</label>
-        <input onChange={ (event) => handleOnChangePhoneInput(event.target.value) } className="w-full md:w-72 max-w-72 rounded-full py-1 px-3" type="number" name="phone" value={ phone } />
+        <label className="text-white text-xl" htmlFor="pForm">Celular</label>
+        <input required autoComplete="off" onChange={ (event) => handleOnChangePhoneInput(event.target.value) } className="w-full md:w-72 max-w-72 rounded-full py-1 px-3" type="number" name="pForm" value={ phone } />
       </div>
       <div className="flex flex-wrap md:flex-nowrap justify-between gap-4">
-        <label className="text-white text-xl" htmlFor="email">Correo</label>
-        <input onChange={ (event) => handleOnChangeEmailInput(event.target.value) } className="w-full md:w-72 max-w-72 rounded-full py-1 px-3" type="email" name="email" value={ email } />
+        <label className="text-white text-xl" htmlFor="mForm">Correo</label>
+        <input required autoComplete="off" onChange={ (event) => handleOnChangeEmailInput(event.target.value) } className="w-full md:w-72 max-w-72 rounded-full py-1 px-3" type="email" name="mForm" value={ email } />
       </div>
       <div className="flex flex-wrap md:flex-nowrap flex-col justify-between gap-4">
         <label className="text-white text-xl" htmlFor="message">Mensaje</label>
-        <textarea onChange={ (event) => handleOnChangeMessageInput(event.target.value) } className="rounded-xl py-2 px-2 w-full" name="message" rows={4} value={ message }></textarea>
+        <textarea required autoComplete="off" onChange={ (event) => handleOnChangeMessageInput(event.target.value) } className="rounded-xl py-2 px-2 w-full" name="message" rows={4} value={ message }></textarea>
       </div>
       <div className="flex justify-center items-center mb-4">
         <button className="bg-yellow-500 hover:bg-yellow-400 rounded-xl font-semibold px-4 py-2">Enviar</button>
